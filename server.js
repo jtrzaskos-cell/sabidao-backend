8905d7e81bd470381521cac0b53cb6ae81335f5e
jtrzaskos-cell <j.trzaskos@escola.pr.gov.br>
2026-08-13 01:08:18 +0000

    Corrige declaracao duplicada no server.js

--- server.js
+++ server.js
@@ -16,10 +16,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-const chatRoutes = require('./routes/chat');
-const authRoutes = require('./routes/auth');
-
-
 app.use('/api', chatRoutes);
 app.use('/api', authRoutes);
 
